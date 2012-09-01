5969b0a6cb212ca1b22029584b52b6739787b320
Roland Walker <walker@pobox.com>
2012-09-01 17:20:32 -0400

    header fmt for package managers

--- string-utils.el
+++ string-utils.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012 Roland Walker
 ;;
-;; Author: Roland Walker walker@pobox.com
+;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/string-utils
 ;; URL: http://raw.github.com/rolandwalker/string-utils/master/string-utils.el
 ;; Version: 0.0.3
