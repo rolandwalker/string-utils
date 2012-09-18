7e950137d939d5c1bac0eb1bdaf9f9ce85eda980
Roland Walker <walker@pobox.com>
2012-09-18 15:39:38 -0400

    revert 8ef3e09 against the spirit of stringifying for content

--- expectations/string-utils-test.el
+++ expectations/string-utils-test.el
@@ -107,7 +107,7 @@
        (define-key tester  (kbd "c")  tester2)
        (string-utils-stringify-anything tester nil 'ints-are-chars)))
 
-  (expect "sleeper"
+  (expect "sleep 10"
     (string-utils-stringify-anything (start-process "sleeper" "*sleeper*" "sleep" "10")))
 
   (expect "a b 3"
